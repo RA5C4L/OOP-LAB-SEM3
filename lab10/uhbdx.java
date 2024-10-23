177c32af7002851c57988f6fecd115406fba35ac
RA5C4L <viveknavneet117@gmail.com>
2024-10-23 14:18:13 +0530

    Delete lab10/uhbdx.java

--- lab10/uhbdx.java
+++
@@ -1,5 +0,0 @@
-class InvalidMarksException extends Exception {
-    public InvalidMarksException(String message) {
-        super(message); // Call the parent constructor with the message
-    }
-}
